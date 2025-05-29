1f71b62ed7bfaf8d2784953165e2b7e4a273959c
zihad <azziqrazihad@gmail.com>
2025-05-29 10:38:43 +0700

    add api for mobile app (auth)

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login'])->name('api.login');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('api.logout');
+
+    Route::get('/user', function(Request $request){
+        return $request->user();
+    })->name('api.user');
+});
